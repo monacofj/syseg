a198e447af403dfa8a8c782a1526ef65f1719eaf
Monaco F. J. <monaco@usp.br>
2026-04-10 00:10:51 -0300

    chore: setup basic build automation infrastructure

---
+++ eg/hw/kh.c
@@ -0,0 +1,15 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <https://github.com/monacofj>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ */
+
+/* Behold the original 1978's K&H example. All in small caps, with comma,
+   and a trailing newline (no exclamation mark)! */
+
+main()
+{
+  printf("hello, world\n");
+}
